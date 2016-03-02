7df3d7f67b0b09378b632d598d63918cf13a0255
Geert Barentsen <geert@barentsen.be>
2016-03-02 10:19:27 -0800

    Adds the Category name

--- k2-c9a-late-targets/create-masks.py
+++ k2-c9a-late-targets/create-masks.py
@@ -74,7 +74,7 @@ class CustomApertureFile(object):
     def write(self, output_fn):
         """Write the custom apertures to the format required by Kepler."""
         with open(output_fn, "w") as out:
-            out.write('Category: Ongoing_Microlens_Events\n')
+            out.write('Category: Ongoing_Microlens_Events_C9a\n')
             for mask in self.masks:
                 out.write('NEW|{skygroup:d}|{flags}|{row}|{col}|{mask}\n'.format(**mask))
 
